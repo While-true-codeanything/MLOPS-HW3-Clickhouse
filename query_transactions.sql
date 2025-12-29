8bab3c9a83d3c0b890541d8262ed5f65366c3f3b
While-true-codeanything <pavlov.erg@gmail.com>
2025-12-29 13:03:10 +0300

    argMax fix

--- query_transactions.sql
+++ query_transactions.sql
@@ -1,6 +1,6 @@
 SELECT
     us_state,
-    argmax(cat_id, amount) AS top_amount_cat_id,
+    argMax(cat_id, amount) AS top_amount_cat_id,
     MAX(amount) AS max_amount
 FROM homework3.transactions
 GROUP BY us_state
